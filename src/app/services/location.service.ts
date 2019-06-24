c559391c3f4a4c10ac2119d5d722783857f44c40
rbv3 <rbv3@cin.ufpe.br>
2019-06-24 17:42:16 -0300

    comentando o service

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -6,10 +6,14 @@ declare var $: any;
 
 @Injectable()
 export class LocationService {
+  //url do nosso back
   private url = 'https://helpmeback.herokuapp.com'
+  //httpCliente precisa ser importado e colcoado no constructor
   constructor(private http: HttpClient) { }
 
+  //Cria-se uma funçao comum q recebe o corpo da requisição
   getNearAlarms(data) {
+    //chama-se o tipo do metodo, nesse caso post, com a url + o endpoint, e o corpo da requisiçao como parametros
     return this.http.post(this.url + '/get_near_alarms', data)
   }
 }

61c64f7357cf8c3a689c2a777e7045caf66a3699
rbv3 <rbv3@cin.ufpe.br>
2019-06-24 17:39:50 -0300

    limpeza do service

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -11,9 +11,5 @@ export class LocationService {
 
   getNearAlarms(data) {
     return this.http.post(this.url + '/get_near_alarms', data)
-    // console.log('teste')
-    // const axios = require('axios')
-    // const coordinates = await axios.$post('https://helpmeback.herokuapp.com/get_near_alarms', { x: lat, y: long, radius: 10.0, ago: 10 })
-    // return coordinates
   }
 }
